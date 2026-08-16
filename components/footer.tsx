3335ea88acbab607708d350e7e2db31538192391
redzzz2001 <redxzz2001@gmail.com>
2026-08-16 16:43:02 -0300

    refaz portfólio em HTML com estética glass (#3)

--- components/footer.tsx
+++
@@ -1,1 +0,0 @@
-export function Footer(){return <footer><a className="wordmark" href="#top" aria-label="Voltar ao início">ROUT<span>®</span></a><p>Find a better route.</p><p>© ROUT STUDIOS</p></footer>}
